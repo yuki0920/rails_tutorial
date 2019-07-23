41e135be7fb9a89528d8df3233bcdf51526b3769
yuki0920 <ywsep20@gmail.com>
2019-07-24 08:25:44 +0900

    add HomeTest

---
+++ test/integration/following_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class FollowingTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
+end
